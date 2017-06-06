d8979c07ddb3379699e7ecacdcc471759797c64d
jfolleas1 <jacques.folleas@insa-lyon.fr>
2017-06-06 17:24:03 +0200

    read calcul

--- CompCorpus/Program.cs
+++ CompCorpus/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Analyser;
+
 
 namespace CompCorpus
 {
@@ -10,6 +13,25 @@ namespace CompCorpus
     {
         static void Main(string[] args)
         {
+            FileStream file = null;
+            Scanner scn = null;
+            Parser parser = null;
+
+            try
+            {
+                file = new FileStream(@"C:\Users\j.folleas\Documents\testGplex\truc\fichierAParser.txt", FileMode.Open);
+                scn = new Scanner(file);
+                parser = new Parser(scn);
+                parser.Parse();
+
+                Console.WriteLine("FIN");
+                Console.ReadLine();
+
+            }
+            finally
+            {
+                file.Close();
+            }
         }
     }
 }
