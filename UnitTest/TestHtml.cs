5caf78a78bdc07974f5f8baa3ad6cd3334344218
jfolleas1 <jacques.folleas@insa-lyon.fr>
2017-06-16 16:37:59 +0200

    add somme tets

---
+++ UnitTest/TestHtml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace UnitTest
+{
+
+    [TestClass]
+    public class TestHtml
+    {
+        [TestMethod]
+        public void TestSimpleDeadText()
+        { 
+            string fileName = "TestSimpleDeadText";
+
+            bool sameFiles = true;
+
+            string srcFilePath = @"C:\Users\j.folleas\Desktop\Tests\src\" + fileName + ".txt";
+            string trgHtmlFilePath = @"C:\Users\j.folleas\Desktop\Tests\trg\" + fileName + ".html";
+            string trgJSFilePath = @"C:\Users\j.folleas\Desktop\Tests\trg\" + fileName + ".js";
+            string resHtmlFilePath = @"C:\Users\j.folleas\Desktop\Tests\res\" + fileName + ".html";
+            string resJSFilePath = @"C:\Users\j.folleas\Desktop\Tests\res\" + fileName + ".js";
+            string[] args = { srcFilePath, trgHtmlFilePath, trgJSFilePath };
+
+            MainTest.TestMain(args);
+            try
+            {   // Open the text file using a stream reader.
+                String linetrgHtml;
+                String lineresHtml;
+                String linetrgJS;
+                String lineresJS;
+
+                using (StreamReader srTrgHtml = new StreamReader(trgHtmlFilePath))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    linetrgHtml = srTrgHtml.ReadToEnd();
+                }
+                using (StreamReader srResHtml = new StreamReader(resHtmlFilePath))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    lineresHtml = srResHtml.ReadToEnd();
+                }
+                using (StreamReader srTrgJS = new StreamReader(trgJSFilePath))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    linetrgJS = srTrgJS.ReadToEnd();
+                }
+                using (StreamReader srResJS = new StreamReader(resJSFilePath))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    lineresJS = srResJS.ReadToEnd();
+                }
+                sameFiles &= (linetrgHtml == lineresHtml);
+                sameFiles &= (linetrgJS == lineresJS);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+
+            Assert.AreEqual(true, sameFiles);
+        }
+    }
+}
