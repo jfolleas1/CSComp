75d7371f7131ac352b0cc4e675051734efc6db35
jfolleas1 <jacques.folleas@insa-lyon.fr>
2017-06-23 16:40:29 +0200

    add include, need to be test

---
+++ CompCorpus/RunTime/PreProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CompCorpus.RunTime
+{
+    static public class PreProcessor
+    {
+        static public void AddIncludes(string fileName)
+        {
+            string includePattern = @"$include *\w+\.\w+;";
+            MatchCollection includesMatches;
+            Regex includeRegex = new Regex(includePattern);
+            includesMatches = propositionRegex.Matches(file);
+            // Iterate matches
+            for (int ctr = 0; ctr < propositionMatches.Count; ctr++)
+            {
+            }
+    }
+}
