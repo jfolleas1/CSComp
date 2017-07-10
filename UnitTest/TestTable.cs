73d93d7adb7e09cc6d0c6ca010dc1ffea7c78477
jfolleas1 <jacques.folleas@insa-lyon.fr>
2017-07-10 17:23:31 +0200

    add test

---
+++ UnitTest/TestTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    /// <summary>
+    /// Description résumée pour TestTable
+    /// </summary>
+    [TestClass]
+    public class TestTable
+    {
+        [TestMethod]
+        public void TestTableSimple()
+        {
+            TestHtml.mainTestHtml("TestTableSimple");
+        }
+
+      
+    }
+}
